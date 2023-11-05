2e312727212cf5b4832a060c1123ef43261053a2
Henri Tikkanen <64799100+henritik@users.noreply.github.com>
2023-11-05 21:58:25 +0200

    Delete old js-directory

--- js/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '1f1effc2f5ebc3d5d231');
--- js/me-block.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '14cefc42a895e05caa44');
