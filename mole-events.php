e7a7544f8bc7485eaf1ac0fcc8e91054c7dce55e
Henri Tikkanen <64799100+henritik@users.noreply.github.com>
2023-11-05 21:56:27 +0200

    Minor changes

--- mole-events.php
+++ mole-events.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Mole Events
 Description: A simple and straightforward events plugin with customizable widgets and shortcodes.
-Version: 0.1.0
+Version: 0.2.0
 Author: Henri Tikkanen
 Author URI: https://github.com/henritik/
 Requires PHP: 7.4
@@ -19,24 +19,25 @@ require_once('includes/mole-events-widget.php');
 require_once('includes/mole-events-admin.php');
 
 function me_plugin_init() {
-	wp_register_script(
-		'me-custom-postmeta',
-		plugin_dir_url( __FILE__ ) . 'js/index.js', 
-		array(
-			'wp-edit-post'
-		)
-	);
-	load_plugin_textdomain( 'mole-events', false, false . '/languages' ); 
+    wp_register_script(
+        'me-custom-postmeta',
+        plugins_url( 'js/index.js', __FILE__ ),
+        array( 'wp-edit-post' )
+    );
+	wp_register_style(
+        'mole-events-css',
+        plugins_url( 'css/mole-events.css', __FILE__ )
+    );
 }
 add_action( 'init', 'me_plugin_init' );
 
-function sidebar_plugin_script_enqueue() {
+function me_script_enqueue() {
 	wp_enqueue_script( 'me-custom-postmeta' );
 }
-add_action( 'enqueue_block_editor_assets', 'sidebar_plugin_script_enqueue' );
+add_action( 'enqueue_block_editor_assets', 'me_script_enqueue' );
 
 function me_assets() {
-	wp_enqueue_style( 'mole-events-css', plugin_dir_url(__FILE__) . 'css/mole-events.css');
+	wp_enqueue_style( 'mole-events-css' );
 }
 add_action('wp_enqueue_scripts', 'me_assets');
 
